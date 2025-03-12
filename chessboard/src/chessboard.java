d89717fe1e201065d6f3a484730e4a5b3fcc0747
Megharaj45 <megharajjayi.ai23@bmsce.ac.in>
2025-03-12 21:55:05 +0530

    Add files via upload

---
+++ chessboard/src/chessboard.java
@@ -0,0 +1,23 @@
+public class chessboard {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("_");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+// *_*_*_*_
+// _*_*_*_*
+// *_*_*_*_
+// _*_*_*_*
+// *_*_*_*_
+// _*_*_*_*
+// *_*_*_*_
+// _*_*_*_*
